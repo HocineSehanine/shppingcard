88c4fa80b13a0421cce452dfb8b66d2fbfc3c3a7
Hocine Sehanine <hocinesehanine1@gmail.com>
2022-05-22 23:48:34 -0300

    req 10/11

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,6 +1,6 @@
 const getSavedCartItems = () => {
   document.querySelector('.cart__items').innerHTML = localStorage.getItem('carrinho');
-  document.querySelector('.cart__items').innerHTML = localStorage.getItem('preço');
+  document.querySelector('.total-price').innerHTML = localStorage.getItem('preço');
 };
 
 if (typeof module !== 'undefined') {
